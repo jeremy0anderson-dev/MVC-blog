82de3644ddd98c0ea3fb053580a29b909d044160
jeremy0anderson <projects@jeremyanderson.dev>
2022-05-23 06:59:01 -0600

    added some controllers / imported to respective routes. Also added seeds to test post/user/comment data with

--- seeds/index.js
+++ seeds/index.js
@@ -1,9 +1,8 @@
 const seedUsers = require('./user-seeds');
 const seedPosts = require('./post-seeds');
 const seedComments = require('./comment-seeds');
-const seedVotes = require('./vote-seeds');
 
-const sequelize = require('../config/connection');
+const sequelize = require('../config');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
@@ -16,10 +15,6 @@ const seedAll = async () => {
 
   await seedComments();
   console.log('--------------');
-
-  await seedVotes();
-  console.log('--------------');
-
   process.exit(0);
 };
 
