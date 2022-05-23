640ccf867e23812d1e15a2784369f6fc477c63b1
jeremy0anderson <projects@jeremyanderson.dev>
2022-05-23 05:49:15 -0600

    create sequelize connection && server

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express'),
+      session = require('express-session'),
+      SessionStore = require('connect-session-sequelize')(session.Store),
+      PORT = process.env.PORT || 4005,
+      app = express();
+
+
+const sequelize = require('./config'),
+      exphbs = require('express-handlebars');
+// Configure view engine as handlebars
+
+app.engine("hbs", exphbs.engine({
+    extname: 'hbs',
+    defaultLayout: "default"
+}))
+app.set('views', './views');
+app.set('view engine', 'hbs');
+
+
+
+// Middleware
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+//configure session
+app.use(session({
+    name:"sid",
+    secret: process.env.SESSION_SECRET || "shhh",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: 1000*60*60*24,
+        secure: process.env.NODE_ENV === "production"
+    },
+    proxy: process.env.NODE_ENV === "production",
+    store: new SessionStore({
+        db: sequelize
+    })
+}));
+
+sequelize.sync({force: false}).then(()=>{
+    app.listen(PORT, ()=>{
+        console.log(`Listening on port: ${PORT}`)
+    })
+})
